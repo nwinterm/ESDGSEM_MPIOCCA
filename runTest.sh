fadd91a6cabd027dab1120ae5280e8a4e26d9b74
nwinterm <nwinterm@math.uni-koeln.de>
2017-09-27 17:01:22 +0200

    fixed bash script to correctly append outputs to a file

--- runTest.sh
+++ runTest.sh
@@ -3,5 +3,7 @@
 #nohup nvprof --force-overwrite --log-file ReadableOutput.log --kernels VolumeKernelFluxDiff0 --metrics flop_sp_efficiency,achieved_occupancy,flop_count_sp,dram_read_throughput,dram_write_throughput ./main CUDA Nelem 100 N 5 Nepad 4 KernelV 1 STDKernelV 1 >Results.txt & disown
 #nohup nvprof --kernels VolumeKernelFluxDiff0 --metrics flop_sp_efficiency,achieved_occupancy,flop_count_sp,dram_read_throughput,dram_write_throughput ./main CUDA Nelem 100 N 5 Nepad 4 KernelV 0 STDKernelV 0 >Results1.txt & disown
 #nohup nvprof --kernels VolumeKernelFluxDiff0 --metrics flop_sp_efficiency,achieved_occupancy,flop_count_sp,dram_read_throughput,dram_write_throughput ./main CUDA Nelem 100 N 5 Nepad 4 KernelV 1 STDKernelV 1 >Results2.txt & disown
-./main CUDA Nelem 3000 N 1 Nepad 32 KernelV 10 STDKernelV 7 >Results.txt
-./main CUDA Nelem 2000 N 2 Nepad 16 KernelV 10 STDKernelV 7 >Results.txt
+VerFD=9
+VerSD=7
+./main CUDA Nelem 3000 N 1 Nepad 32 KernelV VerFD STDKernelV VerSD &>Results.txt 
+./main CUDA Nelem 2000 N 2 Nepad 16 KernelV VerFD STDKernelV VerSD &>>Results.txt 
