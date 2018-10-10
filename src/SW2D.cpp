5426dd07794a237a19395aff0f14cc13af4ce18a
Niklas Wintermeyer <nwinterm@math.uni-koeln.de>
2018-10-10 16:10:47 +0200

    Update src/SW2D.cpp

--- src/SW2D.cpp
+++ src/SW2D.cpp
@@ -758,10 +758,8 @@ void SW2D::InitQNodal(const dfloat x,const dfloat y, dfloat q[],const dfloat t,c
 
     case 90:      /// Tsunami with okada  all wet (for meter mesh now)
     {
-        //dfloat zero = 0.0;
-        //h=max(zero,h_0-b);
-        dfloat two = 2000.0;
-        h=h_0+2000.0-b;
+
+        h=h_0+20000.0-b;
         v= 0.0;
         w= 0.0;
         break;
