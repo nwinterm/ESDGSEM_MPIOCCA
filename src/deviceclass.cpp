927f74d9d24149464a5bf5a306c81b1652efa339
Niklas Wintermeyer <nwinterm@math.uni-koeln.de>
2018-10-23 20:14:02 +0200

    Update src/deviceclass.cpp

--- src/deviceclass.cpp
+++ src/deviceclass.cpp
@@ -811,11 +811,11 @@ void deviceclass:: DGtimeloop(const int Nelem,
                     id = floor(chennaiID/ngl2)*(Neq-1)*ngl2 + chennaiID;
                     ChennaiTimeSeries[plotCount] = q_global[id ]+b_global[chennaiID]-h_0;
                     id = floor(tuticorinID/ngl2)*(Neq-1)*ngl2 + tuticorinID;
-                    TuticorinTimeSeries[plotCount]= q_global[(tuticorinID)*Neq+tuticorinID]+b_global[tuticorinID]-h_0;
+                    TuticorinTimeSeries[plotCount]= q_global[id ]+b_global[tuticorinID]-h_0;
                     id = floor(viskhapatnamID/ngl2)*(Neq-1)*ngl2 + viskhapatnamID;
-                    VisakhapatnamTimeSeries[plotCount]= q_global[(viskhapatnamID)*Neq+viskhapatnamID]+b_global[viskhapatnamID]-h_0;
+                    VisakhapatnamTimeSeries[plotCount]= q_global[id ]+b_global[viskhapatnamID]-h_0;
                     id = floor(paradipID/ngl2)*(Neq-1)*ngl2 + paradipID;
-                    ParadipTimeSeries[plotCount]= q_global[(paradipID)*Neq+paradipID]+b_global[paradipID]-h_0;
+                    ParadipTimeSeries[plotCount]= q_global[id ]+b_global[paradipID]-h_0;
                     TimeSeriesTimes[plotCount]=t;
 
                 }
@@ -1153,10 +1153,15 @@ void deviceclass:: DGtimeloop(const int Nelem,
                     }
                     if (createTimeSeries)
                     {
-                        ChennaiTimeSeries[plotCount] = q_global[(chennaiID-1)*Neq+chennaiID]+b_global[chennaiID]-h_0;
-                        TuticorinTimeSeries[plotCount]= q_global[(tuticorinID-1)*Neq+tuticorinID]+b_global[tuticorinID]-h_0;
-                        VisakhapatnamTimeSeries[plotCount]= q_global[(viskhapatnamID-1)*Neq+viskhapatnamID]+b_global[viskhapatnamID]-h_0;
-                        ParadipTimeSeries[plotCount]= q_global[(paradipID-1)*Neq+paradipID]+b_global[paradipID]-h_0;
+                        int id;
+                        id = floor(chennaiID/ngl2)*(Neq-1)*ngl2 + chennaiID;
+                        ChennaiTimeSeries[plotCount] = q_global[id ]+b_global[chennaiID]-h_0;
+                        id = floor(tuticorinID/ngl2)*(Neq-1)*ngl2 + tuticorinID;
+                        TuticorinTimeSeries[plotCount]= q_global[id ]+b_global[tuticorinID]-h_0;
+                        id = floor(viskhapatnamID/ngl2)*(Neq-1)*ngl2 + viskhapatnamID;
+                        VisakhapatnamTimeSeries[plotCount]= q_global[id ]+b_global[viskhapatnamID]-h_0;
+                        id = floor(paradipID/ngl2)*(Neq-1)*ngl2 + paradipID;
+                        ParadipTimeSeries[plotCount]= q_global[id ]+b_global[paradipID]-h_0;
                         TimeSeriesTimes[plotCount]=t;
 
                     }
